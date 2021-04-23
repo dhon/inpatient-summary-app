12d3dd988f8fe93402e6c8589182f779c921f695
Dhonovan Hauserman <morgangrimes14@gmail.com>
2021-04-23 13:59:36 -0500

    ask-a-question form logic

--- src/app/components/ask-a-question/ask-a-question.component.ts
+++ src/app/components/ask-a-question/ask-a-question.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: "app-ask-a-question",
   templateUrl: "./ask-a-question.component.html",
   styleUrls: ["./ask-a-question.component.scss"],
 })
-export class AskAQuestionComponent implements OnInit {
-  constructor() {}
+export class AskAQuestionComponent {
+  isValidFormSubmitted = false;
+  topics = ['Care', 'Doctor', 'Facility', 'Other'];
+  questionForm = this.formBuilder.group({
+     topic: ['', Validators.required],
+     email: ['', Validators.email],
+     question: ['', Validators.required]
+  });
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    if (this.questionForm.valid) {
+      // API Request to Company Database
+      this.isValidFormSubmitted = true;
+    }
+ }
 }
